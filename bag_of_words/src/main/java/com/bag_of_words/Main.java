481d7ef4a2d31dcd144839d2bf08eb6ffd15f824
KyangTan <kwanyang29@gmail.com>
2024-05-06 17:52:39 +0800

    First Commit

---
+++ bag_of_words/src/main/java/com/bag_of_words/Main.java
@@ -0,0 +1,81 @@
+package com.bag_of_words;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+public class Main {
+
+    // private static final String INPUT_FILE = "war_and_peace.txt";
+    static InputStream in = Main.class.getResourceAsStream("/war_and_peace.txt"); 
+
+    private static final int NUMBER_OF_THREADS = 8;
+
+    public static void main(String[] args) throws IOException {
+
+        //Change the word happy to search for different word
+        //http://localhost:8000/search?word=happy
+        String text = new String(in.readAllBytes());
+        // String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+        startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/search", new WordCountHandler(text));
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        server.setExecutor(executor);
+        server.start();
+    }
+
+    private static class WordCountHandler implements HttpHandler {
+
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+            if (!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            long count = countWord(word);
+
+            byte[] response = Long.toString(count).getBytes();
+            httpExchange.sendResponseHeaders(200, response.length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
+        }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+            while (index >= 0) {
+                index = text.indexOf(word, index);
+
+                if (index >= 0) {
+                    count++;
+                    index++;
+                }
+            }
+            return count;
+        }
+    }
+}
